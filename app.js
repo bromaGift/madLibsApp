5c939eaca9f2420ea0c1d389df94ad9ffd8761ff
Broma Gift <elenwogift2@gmail.com>
2022-10-19 19:35:00 +0100

    Added declarations of mad-lib replacement variables

---
+++ app.js
@@ -0,0 +1,34 @@
+var originalStory = `<h1>I ${userInputs[0]} the first quarter of a class in middle school, 
+  so I made a fake report card. I did this every quarter that year. 
+  I forgot that they ${userInputs[1]} home the end-of-year cards, and my mom got it before I could intercept 
+  with my fake. She was ${userInputs[2]} the school for their error. The ${userInputs[3]} also retired that year and
+   had already ${userInputs[4]} out his records, so they had to take my ${userInputs[5]} “proof” 
+   (the fake ones I made throughout the year) and ${userInputs[6]} the ${userInputs[7]} 
+   I have never told her the truth.</h1>`;
+
+console.log(originalStory);
+
+let userPrompt = "please enter ";
+let userInputs = [
+  "a verb",
+  "a verb",
+  " an adjective",
+  "a proffession",
+  "a verb",
+  "a noun",
+  "an adjective",
+  "a verb",
+];
+
+var numberOfQuestions = 7;
+
+var questionArray = [];
+
+//    var originalStory =
+//   `I failed the first quarter of a class in middle school,
+//   so I made a fake report card. I did this every quarter that year.
+//   I forgot that they mail home the end-of-year cards, and my mom got it before I could intercept
+//   with my fake. She was PISSED at the school for their error. The teacher also retired that year and
+//    had already thrown out his records, so they had to take my mother’s “proof”
+//    (the fake ones I made throughout the year) and “correct” the “mistake.”
+//    I have never told her the truth.`;
